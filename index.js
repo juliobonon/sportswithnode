aa3096d296a405fad6491f429df0a9f1490480de
Julio Cesar Bonon <juliocesarbonon@gmail.com>
2021-06-07 11:02:53 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 var mysql = require('mysql2');
 
-function inserir(esporte) {
+function readJson() {
+  const fs = require('fs');
+  var text = fs.readFileSync('./settings.json', 'utf8');
+  var data = JSON.parse(text);
+
+  return [data.user, data.password];
+}
+
+function inserir(esporte, user, password) {
   var con = mysql.createConnection({
     host: "localhost",
-    user: "root",
-    password: "x@x",
+    user: user,
+    password: password,
     database: "jogos_olimpicos"
   });
 
@@ -15,17 +23,20 @@ function inserir(esporte) {
     const values = [esporte.modalidade, esporte.tipo, esporte.distancia, esporte.sexo, esporte.local];
     con.query(sql, values, function (err, result) {
       if (err) throw err;
-      console.log("1 record inserted");
+      console.log(esporte.modalidade, esporte.local + ": record inserted");
     });
   });
 }
 
-esporte = [{modalidade:"teste1", tipo: "teste2", distancia: "teste3", sexo: "teste4", local: "teste5"},
-{modalidade:"teste1", tipo: "teste2", distancia: "teste3", sexo: "teste4", local: "teste5"},
-{modalidade:"teste1", tipo: "teste2", distancia: "teste3", sexo: "teste4", local: "teste5"},
-{modalidade:"teste1", tipo: "teste2", distancia: "teste3", sexo: "teste4", local: "teste5"},
-{modalidade:"teste1", tipo: "teste2", distancia: "teste3", sexo: "teste4", local: "teste5"}];
+esportes = [
+{modalidade:"Natação", tipo: "teste1", distancia: "teste1", sexo: "teste1", local: "SP"},
+{modalidade:"Karate", tipo: "Luta", distancia: "teste2", sexo: "teste2", local: "RJ"},
+{modalidade:"teste3", tipo: "teste3", distancia: "teste3", sexo: "teste3", local: "teste3"},
+{modalidade:"teste4", tipo: "teste4", distancia: "teste4", sexo: "teste4", local: "teste4"},
+{modalidade:"teste5", tipo: "teste5", distancia: "teste5", sexo: "teste5", local: "teste5"}];
 
-for (var i = 0; i < esporte.length; i++){
-  inserir(esporte[i]);
+var settings = readJson() // le as credenciais
+
+for (var i = 0; i < esporte.length; i++){ //insere todos os dados do vetor de esportes
+  inserir(esportes[i], settings[0], settings[1]);
 }
